1fcdc9dff09db32c9b15ad6be21d008c64ff77d9
Filipe Chagas <filipe.ferraz0@gmail.com>
2023-12-21 11:44:39 -0400

    Update loss.py

--- tensorflow_stnn_lib/loss.py
+++ tensorflow_stnn_lib/loss.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
-def triplet_loss(y_true: tf.Tensor, y_pred: tf.Tensor, margin: float = 0.1) -> tf.Tensor:    
+def triplet_loss(y_true: tf.Tensor, y_pred: tf.Tensor, margin: float = 1.0) -> tf.Tensor:    
     """Triplet Loss function
 
     :param y_true: unused target labels
     :type y_true: tf.Tensor
     :param y_pred: model output (a list with positive and negative distances)
     :type y_pred: tf.Tensor
-    :param margin: desired separation margin between positive and negative examples, defaults to 0.1
+    :param margin: desired separation margin between positive and negative examples, defaults to 1.0
     :type margin: float, optional
     :return: triplet loss tensor
     :rtype: tf.Tensor
